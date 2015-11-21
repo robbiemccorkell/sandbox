05ef7024fc67221b1bc52ec6b7c70a3d7f208623
Robbie McCorkell <robbiemccorkell@gmail.com>
2015-11-21 22:54:37 +0100

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,16 +4,16 @@
 SHA1=$1
 
 # Deploy image to Docker Hub
-docker push circleci/hello:$SHA1
+docker push robbiemccorkell/sandbox:$SHA1
 
 # Create new Elastic Beanstalk version
-EB_BUCKET=sandbox-bucket
+EB_BUCKET=robbiemccorkell-sandbox-bucket
 DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
 sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
 aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
-aws elasticbeanstalk create-application-version --application-name hello \
-  --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
+aws elasticbeanstalk create-application-version --application-name sandbox \
+  --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE --region eu-west-1
 
 # Update Elastic Beanstalk environment to new version
-aws elasticbeanstalk update-environment --environment-name hello-env \
-    --version-label $SHA1
+aws elasticbeanstalk update-environment --environment-name robbiemccorkell-sandbox \
+    --version-label $SHA1 --region eu-west-1
